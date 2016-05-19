9954950d62a2236134235570fccf5a12192a0fd1
calamite963 <lavachequirit@hotmail.ca>
2016-05-19 17:01:52 -0400

    testvero

--- slider.js
+++ slider.js
@@ -1,7 +1,7 @@
 var $slider_data = ' \
 <a \
     href="_img/accueil/01_buffleII.jpg" \
-    title="« Buffle II »" \
+    title="« veroBuffle II »" \
     data-title-href="http://anusornkhabpet.com/anusornkhabpet/buffle2.html" \
     data-etu="Anusorn Khabpet" \
     data-etu-href="http://anusornkhabpet.com/" \
