6f1b214ba149d6d0f7cb020e4a5651cd584b92ad
calamite963 <lavachequirit@hotmail.ca>
2016-05-25 15:22:08 -0400

    Update slider.js

--- slider.js
+++ slider.js
@@ -25,45 +25,13 @@ var $slider_data = ' \
     > \
 </a> \
 <a \
-    href="_img/accueil/03_forme.jpg" \
-    title="« -formé »" \
-    data-title-href="http://bit.ly/paraforme-quebec" \
-    data-etu="Paolo Almario" \
-    data-etu-href="http://paoloalmario.com/" \
-    data-sexe="M" \
-    data-programme="Maîtrise en Art" \
-    data-programme-href="http://programmes.uqac.ca/3848" \
-    data-date="Automne 2013" \
-    data-credits="Photo : Paolo Almario" \
-    data-credits-href="http://www.paoloalmario.com/" \
-    > \
-</a> \
-<a \
-    href="_img/accueil/04_polyptique.jpg" \
-    title="Polyptique E.P." \
-    data-title-href="http://dramaturgiesonore.com/recherches-creations/productions-universitaires/item/146-polyptyque-ep-2013" \
-    data-etu="" \
-    data-etu-href="" \
-    data-sexe="Plu" \
-    data-programme="Cours de production théatrale" \
-    data-programme-href="http://cours.uqac.ca/7ETH232" \
-    data-date="Hiver 2013" \
-    data-credits="Photo : François Verrault" \
-    data-credits-href="" \
-    data-plus="Direction Gilone Brun." \
-    > \
-</a> \
- \
-<a \
-    href="_img/accueil/05_tournage.jpg" \
-    title="La Guerre Noire" \
-    data-etu="Danny Langlois" \
-    data-sexe="M" \
-    data-programme="BIA" \
-    data-programme-href="http://programmes.uqac.ca/7886" \
-    data-date="Hiver 2013" \
-    data-credits="Photo : Laurie-Ann Dufour Guérin" \
-    data-credits-href="http://500px.com/LADG"> \
+    href="http://nsb002.github.io/modarts_slider/hk.jpg" \
+    title="Le Module des arts à Hong Kong" \
+    data-etu=" " \
+    data-sexe=" " \
+    data-programme=" " \
+    data-title-href="https://www.facebook.com/artsUQAC/?fref=ts" \
+    data-plus="<b>[ACTUALITÉS] 21 mai 2016</b><br>Le groupe de recherche interuniversitaire INSERTIO, du Module des arts est à Hong Kong pour ISEA2016. L\'équipe anime un workshop dans l\'édifice de Zaha Hadid à PolyU à Hong Kong." \
 </a>\
 <a \
     href="http://nsb002.github.io/modarts_slider/HC3.jpg" \
